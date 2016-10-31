dc08c68296d9ca506b2255e59c0b4fa8e34e1779
Josué M. Sehnem <josue@sehnem.com>
2016-10-31 20:57:16 -0200

    Adaptação PEP8

--- pymicra/__init__.py
+++ pymicra/__init__.py
@@ -7,21 +7,23 @@ Pymicra - Python tool for Micrometeorological Analyses
 
 """
 import os
-vfile = os.path.join(os.path.dirname(os.path.realpath(__file__)),'version')
+
+vfile = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'version')
 __version__ = open(vfile, 'rt').read().strip()
 
-#---------
+# ---------
 # Here we create a "global" unitregistry for pymicra
 try:
     from pint.unit import UnitRegistry
     import os
+
     abspath = os.path.dirname(os.path.abspath(__file__))
     ureg = UnitRegistry()
-    ureg.load_definitions(os.path.join(abspath,'pymicra.pint'))
+    ureg.load_definitions(os.path.join(abspath, 'pymicra.pint'))
     Q_ = ureg.Quantity
 except ImportError:
     print('No pint installed yet. Install pint!')
-#---------
+# ---------
 from . import decorators
 
 from .io import *
@@ -41,8 +43,9 @@ from . import methods
 
 notation = Notation()
 
-#---------
+# ---------
 # Some quick pandas display configurations
 import pandas as _pd
-_pd.options.display.max_rows=26
-_pd.options.display.width=160
+
+_pd.options.display.max_rows = 26
+_pd.options.display.width = 160

fb92d4c32890091f16b887aaed832593cb09c30e
Josué M. Sehnem <josue@sehnem.com>
2016-10-31 19:56:54 -0200

    Ajuste espaçamento

---
+++ build/lib/pymicra/algs/__init__.py
@@ -0,0 +1,7 @@
+from . import units
+from . import general
+
+from .auxiliar import *
+from .general import *
+from .numeric import *
+from .units import *
--- setup.py
+++ setup.py
@@ -5,32 +5,35 @@ Python Micrometeorological Analysis tool - Pymicra
 
 import os
 
-#-----------
+# -----------
 # Import version from one file used by the whole module
-vfile = os.path.join(os.path.dirname(os.path.realpath(__file__)),'pymicra/version')
+vfile = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pymicra/version')
 __version__ = open(vfile, 'rt').read().strip()
-#-----------
+# -----------
 
-dependencies=['Pandas==0.17.1', 'Numpy', 'pint==0.7.2']
+dependencies = ['Pandas==0.17.1', 'Numpy', 'pint==0.7.2']
 
-#-----------
+# -----------
 # Be ready to work with or without setuptools
 try:
     from setuptools import setup, find_packages
+
     print("Setuptools installed. Proceeding with installation...\n")
 except ImportError:
-    raise ImportError("No setuptools installed!\nPlease install setuptools in order to proceed with installation. Try: sudo apt-get install python-setuptools")
-#-----------
+    raise ImportError(
+        "No setuptools installed!\n"
+        "Please install setuptools in order to proceed with installation. Try: sudo apt-get install python-setuptools")
+# -----------
 
-#-----------
+# -----------
 # Being sure to find every submodule within pymicra and set up the kwargs
 packages = find_packages()
-#-----------
+# -----------
 
-extra_kwargs={'install_requires' : dependencies}
+extra_kwargs = {'install_requires': dependencies}
 
 setup(name='pymicra',
-      version = __version__,
+      version=__version__,
       description='A Python tool for Micrometeorological Analyses',
       long_description=open('README.rst').read(),
       url='https://github.com/tomchor/pymicra.git',
@@ -38,6 +41,5 @@ setup(name='pymicra',
       author_email='tomaschor@gmail.com',
       license='GNU GPL V3.0',
       packages=packages,
-      package_data={'pymicra':['pymicra.pint', 'version']},
+      package_data={'pymicra': ['pymicra.pint', 'version']},
       **extra_kwargs)
-
